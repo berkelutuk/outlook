32c7cbffb3b55ba68383ad9bf4da6ba1b5ef591f
berkelutuk <141685187+berkelutuk@users.noreply.github.com>
2023-08-08 01:55:11 +0330

    Update index.js

--- constants/index.js
+++ constants/index.js
@@ -1,4 +1,4 @@
-export const TOKEN_CONTRACT_ADDRESS = "0xfF5cC26d33A84F10760718b207B1765744D0fB0A";
+export const TOKEN_CONTRACT_ADDRESS = "0xe96cc870e8b4769ac6c574f951b45b7c068be9e2";
 export const TOKEN_CONTRACT_ABI = [
 	{
 		"anonymous": false,
